25d0fb5cb4fed0dd07e7d6dd0bbf287fc087e00f
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2021-08-25 22:52:35 +0900

    story content layout css 손보기 - resize, image slide를 반응형으로

--- srcs/frontend/js/storySlide.js
+++ srcs/frontend/js/storySlide.js
@@ -1,76 +1,88 @@
-const slideWrap = document.querySelector('.story__content-slide-wrap');
-const slideBox = document.querySelector('.story__content-slide-box');
-const slideList = document.querySelector('.story__content-slide-list');
-const slideContent = document.querySelectorAll('.story__content-slide-content');
-const slidePrevBtn = document.querySelector('.story__content-slide-prevBtn');
-const slideNextBtn = document.querySelector('.story__content-slide-nextBtn');
-const slidePage = document.querySelector('.story__content-slide-pagination');
+const renderStoryContent = () => {
 
-// Slide List
-const slideWidth = 480;
-const slideLen = slideContent.length;
-
-slideList.style.width = slideWidth * slideLen + "px";
-
-
-// Slide Function
-let index = 0;
-
-
-// Pagination
-let pageDot = '';
-for (let i = 0; i < slideLen; i++) {
-    pageDot += `
-        <li class='dot' data-index="${i}"></li>
-    `
-}
-slidePage.innerHTML = pageDot;
-
-slidePage.querySelectorAll('.dot')[0].classList.add('dot-active');
-const pageDotActive = () => {
-    const dot_active = slidePage.querySelector('.dot-active');
-    console.log(dot_active);
-    const dots = slidePage.querySelectorAll('.dot')
+    const slideWrap = document.querySelector('.story__content-slide-wrap');
+    const slideBox = document.querySelector('.story__content-slide-box');
+    const slideList = document.querySelector('.story__content-slide-list');
+    const slideContent = document.querySelectorAll('.story__content-slide-content');
+    const slidePrevBtn = document.querySelector('.story__content-slide-prevBtn');
+    const slideNextBtn = document.querySelector('.story__content-slide-nextBtn');
+    const slidePage = document.querySelector('.story__content-slide-pagination');
     
-    dots.forEach((element) => {
-        if (parseInt(element.dataset.index) === index) {
-            dot_active.classList.remove('dot-active');
-            element.classList.add("dot-active");
-        }
-    });
-}
-
-
-const slideNext = () => {
-    index++;
-    pageDotActive();
-    if (index !== 0) {
-        slidePrevBtn.style.display = "inline-block"
-    }
-    if (index <= slideLen - 1) {
-        slideList.style.transform = `translateX(-${slideWidth * index}px)`;
-        slideList.style.transition = `300ms`;
-    }
-    if (index === slideLen - 1) {
+    // Slide List
+    let slideWidth = Math.floor(slideBox.offsetWidth);
+    const slideLen = slideContent.length;
+    slideContent.forEach((element => {
+        element.style.cssText += `
+            padding-bottom: ${100 / slideLen}%
+        `
+    }))
+    slideList.style.width = slideWidth * slideLen + "px";
+    
+    // 사진이 1장일 때
+    if (slideLen === 1) {
         slideNextBtn.style.display = "none";
     }
-}
-const slidePrev = () => {
-    index--;
-    pageDotActive();
-    if (index === 0) {
-        slidePrevBtn.style.display = "none"
+    
+    
+    // Slide Function
+    let index = 0;
+    
+    
+    // Pagination
+    let pageDot = '';
+    for (let i = 0; i < slideLen; i++) {
+        pageDot += `
+            <li class='dot' data-index="${i}"></li>
+        `
     }
-    if (index >= 0) {
-        slideList.style.transform = `translateX(-${slideWidth * index}px)`;
-        slideList.style.transition = `300ms`;
+    slidePage.innerHTML = pageDot;
+    
+    slidePage.querySelectorAll('.dot')[0].classList.add('dot-active');
+    const pageDotActive = () => {
+        const dot_active = slidePage.querySelector('.dot-active');
+        console.log(dot_active);
+        const dots = slidePage.querySelectorAll('.dot')
+        
+        dots.forEach((element) => {
+            if (parseInt(element.dataset.index) === index) {
+                dot_active.classList.remove('dot-active');
+                element.classList.add("dot-active");
+            }
+        });
     }
-    if (index !== slideLen - 1) {
-        slideNextBtn.style.display = "inline-block";
+    
+    const slideNext = () => {
+        index++;
+        pageDotActive();
+        if (index !== 0) {
+            slidePrevBtn.style.display = "inline-block"
+        }
+        if (index <= slideLen - 1) {
+            slideList.style.transform = `translateX(-${slideWidth * index}px)`;
+            slideList.style.transition = `300ms`;
+        }
+        if (index === slideLen - 1) {
+            slideNextBtn.style.display = "none";
+        }
     }
-}
+    const slidePrev = () => {
+        index--;
+        pageDotActive();
+        if (index === 0) {
+            slidePrevBtn.style.display = "none"
+        }
+        if (index >= 0) {
+            slideList.style.transform = `translateX(-${slideWidth * index}px)`;
+            slideList.style.transition = `300ms`;
+        }
+        if (index !== slideLen - 1) {
+            slideNextBtn.style.display = "inline-block";
+        }
+    }
+    
+    slidePrevBtn.addEventListener("click", slidePrev);
+    slideNextBtn.addEventListener("click", slideNext);
+}    
 
-
-
-slidePrevBtn.addEventListener("click", slidePrev);
-slideNextBtn.addEventListener("click", slideNext);
+renderStoryContent();
+window.addEventListener('resize', renderStoryContent);
