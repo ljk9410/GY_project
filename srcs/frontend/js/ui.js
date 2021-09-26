ff91c5e44cc97e9fac7a0abb85a5e7416ac536b2
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2021-09-26 15:58:40 +0900

    sidebar 및 main 화면 세부조정

--- srcs/frontend/js/ui.js
+++ srcs/frontend/js/ui.js
@@ -1,6 +1,8 @@
 const navToggleBtn = document.querySelector(".menu__toggle");
 const navContainer = document.querySelector(".nav__container");
+const mainContainer = document.querySelector(".main__container");
 
 navToggleBtn.addEventListener("click", () => {
-    navContainer.classList.toggle("nav-active");
+    navContainer.classList.toggle("nav-active__header");
+    mainContainer.classList.toggle("nav-active__main");
 })
