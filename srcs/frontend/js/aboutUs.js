1dd6e010460da7138c97f0fa1eca2a609ddefb03
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2021-11-04 16:47:11 +0900

    about us scroll-snap 기능 실험

--- srcs/frontend/js/aboutUs.js
+++ srcs/frontend/js/aboutUs.js
@@ -1,11 +1,59 @@
-window.addEventListener('scroll', (e) => {
+const container = document.querySelector('.abountUs__container');
+
+container.addEventListener('scroll', (e) => {
     const sections = document.querySelectorAll('.aboutUs__section');
     const sectionPosition = sections[1].getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 3;
 
+    console.log(sectionPosition, screenPosition);
     if (sectionPosition < screenPosition) {
         sections[1].classList.add('scroll-active');
     }
     else
         sections[1].classList.remove('scroll-active');
 })
+
+// typing effect
+const typedTextSpan = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const textArray = ["선후배", "연결고리", "술?", "사랑"];
+const typingDelay = 150;
+const erassingDelay = 80;
+const newTextDelay = 800;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursor.classList.contains("typing"))
+        cursor.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erassingDelay);
+    } else {
+        cursor.classList.remove("typing");
+        textArrayIndex++;
+        // 무한반복
+        // if (textArrayIndex >= textArray.length)
+        //     textArrayIndex = 0;
+        setTimeout(type, typingDelay + 800);
+    }
+}
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursor.classList.contains("typing"))
+            cursor.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursor.classList.remove("typing");
+        if (textArrayIndex >= textArray.length - 1)
+            return ;
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+type();
